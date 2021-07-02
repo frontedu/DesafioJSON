ec4032f81ab16d69ffd4db2a86905c83f9e4071e
Eduardo F. Silva <frontedu@icloud.com>
2021-07-02 12:13:12 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,182 @@
+//DADOS PARA O FORMUÁRIO
+let data = [
+{
+   "name":"faleconosco",
+   "method":"get",
+   "action":"enviado.html",
+   "autocomplete":true,
+   "items":[
+      {
+         "element":"input",
+         "type":"text",
+         "name":"nome",
+         "placeholder":"Digite seu nome",
+         "id":"nome",
+         "prelabel":"Nome: "
+      },
+      {
+         "element":"input",
+         "type":"radio",
+         "value":"ADS",
+         "name":"curso",
+         "id":"ads",
+         "poslabel":"Análise e Desenvolvimento de Sistemas"
+      },
+      {
+         "element":"input",
+         "type":"radio",
+         "value":"EC",
+         "name":"curso",
+         "id":"ec",
+         "poslabel":"Engenharia de Computação"
+      },
+      {
+         "type":"select",
+         "name":"turno",
+         "children":[
+            {
+               "element":"option",
+               "value":"noturno",
+               "innerhtml":"Noturno"
+            },
+            {
+              "element":"option",
+              "value":"vespertino",
+              "innerhtml":"Vespertino"
+           },
+            {
+               "element":"option",
+               "value":"multi",
+               "innerhtml":"Multiperiódico"
+            }
+         ],
+         "prelabel":"Período: "
+      },
+      {
+         "element":"input",
+         "type":"submit",
+         "value":"Enviar",
+         "name":"enviar"
+      }
+   ]
+}
+];
+
+let input;
+//GERAR FORMULÁRIO
+function generate() {
+  const prepform =
+    '<form name="' +
+    data[0].name +
+    '" action="' +
+    data[0].action +
+    '" method="' +
+    data[0].method +
+    '" autocomplete="' +
+    data[0].autocomplete +
+    '">';
+  document.write(prepform);
+  byIndex();
+  const appform = "</form>";
+  document.write(appform);
+}
+
+//GERAR CAMPOS DO FORMULÁRIO
+function byIndex() {
+  for (index of data[0].items) {
+    index.element == "select" ? (index.type = "select") : false;
+    switch (index.type) {
+      case "text":
+        document.write(isText(index));
+        break;
+      case "radio":
+        document.write(isRadio(index));
+        break;
+      case "select":
+        document.write(isSelect(index));
+        break;
+      case "checkbox":
+        document.write(isCheckbox(index));
+        break;
+      case "submit": case "button":
+        document.write(isSubmit(index));
+        break;
+    }
+  }
+}
+
+//GERAR CAMPOS DE INPUT TEXTO
+let isText = (obj) => {
+  input = '<label for="' + obj.id + '">';
+  obj.prelabel ? (input += obj.prelabel) : false;
+  input +=
+    '</label><input type="text" id="' +
+    obj.id +
+    '" name="' +
+    obj.name +
+    '" placeholder="' +
+    obj.placeholder +
+    '"><br></br>';
+  return input;
+};
+
+//GERAR CAMPOS DE INPUT RADIO
+let isRadio = (obj) => {
+  input =
+    '<input type="radio" id="' +
+    obj.id +
+    '" name="' +
+    obj.name +
+    '" value="' +
+    obj.value +
+    '"><label for="' +
+    obj.id +
+    '">' +
+    obj.poslabel +
+    "</label><br>";
+  return input;
+};
+
+//GERAR CAMPOS DE DROP-DOWN
+let isSelect = (obj) => {
+  input = '<label for="' + obj.name + '">';
+  obj.prelabel ? (input += obj.prelabel) : false;
+  input += '</label><select name="' +
+  obj.name +'" id="' + obj.name + '">';
+  for (option of index.children) {
+    input +=
+      '<option value="' + option.value + '">' + option.innerhtml + "</option>";
+  }
+  input += "</select><br><br>";
+  return input;
+};
+
+//GERAR CAMPOS DE CHECKBOX
+let isCheckbox = (obj) => {
+  input =
+    '<input type="checkbox" id="' +
+    obj.id +
+    '" name="' +
+    obj.name +
+    '" value="' +
+    obj.value +
+    '"><label for="' +
+    obj.id +
+    '">' +
+    obj.poslabel +
+    "</label><br>";
+  return input;
+};
+
+//GERAR CAMPO SUBMIT
+let isSubmit = (obj) => {
+  input =
+    '<br><input type="' +
+    obj.type +
+    '" name="' +
+    obj.name +
+    '" value="' +
+    obj.value +
+    '">';
+  return input;
+};
